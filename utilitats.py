1b1ab78c5891a1e8aa23da0d82fa827df174a57c
jordi <jordi.miserachs@gmail.com>
2024-11-13 17:37:22 +0100

    intent 1 de posar en producció

--- utilitats.py
+++ utilitats.py
@@ -4,4 +4,5 @@ from app import create_app, socketioApp
 app = create_app()
 
 if __name__ == '__main__':
-    socketioApp.run(app, host='0.0.0.0', port=5000)
+    # socketioApp.run(app, host='0.0.0.0', port=5000)    
+    socketioApp.run(app)
